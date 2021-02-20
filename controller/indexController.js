7f979e042eb3ca9cba4cb46117721d8e836a7cbe
olmedo <olmedito.90@gmail.com>
2021-02-20 01:15:13 -0400

    cambio simple

--- controller/indexController.js
+++ controller/indexController.js
@@ -2,7 +2,7 @@ class IndexController {
   constructor() {}
   //services
   index(request, response) {
-    response.status(200).json({ msn: "HOLA MUNDO DESDE REST" });
+    response.status(200).json({ msn: "HOLA MUNDO DESDE REST OLMEDO" });
   }
 }
 export default IndexController;
